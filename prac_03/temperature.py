ac64cce53a7a7912725b84289c9e3bd90fc699bb
Shoon <shoonleitun@my.jcu.edu.au>
2019-08-04 23:52:44 +0800

    Temperature from prac 1

--- prac_03/temperature.py
+++ prac_03/temperature.py
@@ -6,6 +6,7 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
